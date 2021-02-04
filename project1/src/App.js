845a085d13a3d78ce6b47eb8cc86e80c85bb9ed0
unknown <mejhasuraj@gmail.com>
2021-02-04 16:56:48 +0530

    prepare for deployment

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,10 +1,46 @@
 import React from "react"
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from "./components/Footer";
+import Header from './components/Header'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
+
 
 const App=()=> {
   return (
-    <>
-      <h1>welcome</h1>
-    </>
+    
+    <Router>
+    
+    
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Route path="/order/:id" component={OrderScreen} />
+        <Route path="/shipping/" component={ShippingScreen} />
+        <Route path="/placeorder/" component={PlaceOrderScreen} />
+        <Route path="/payment/" component={PaymentScreen} />
+        <Route path="/login/" component={LoginScreen} />
+        <Route path="/register/" component={RegisterScreen} />
+        <Route path="/profile/" component={ProfileScreen} />
+        <Route path="/product/:id" component={ProductScreen} />
+        <Route path="/cart/:id?" component={CartScreen} />
+        <Route path="/admin/userlist" component={UserListScreen} />
+        <Route path="/" component={HomeScreen} exact />
+      </Container>
+    </main>
+    <Footer />
+    </Router>
+    
   );
 }
 
